f10fb2c702589ad19750dca0128dd5ad3688136b
Miguel Aroldi <armi@aeis.ch>
2023-04-20 08:48:22 +0200

    fixed auto deploy

--- auto-delpoy.sh
+++ auto-delpoy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pid=ps -ef | grep -E "/root\W{1,}\d{1,}.*\Wpython3\Wmain.py/gm" | grep -E "/\d{3,}/gm"
+pid=$(ps -ef | grep -E "/root\W{1,}\d{1,}.*\Wpython3\Wmain.py/gm" | grep -E "/\d{3,}/gm")
 
 echo "$pid"

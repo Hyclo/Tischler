993d9bc713bd2f487fb9c58f59ae1b878884dc5a
Miguel Aroldi <armi@aeis.ch>
2023-04-20 12:09:03 +0200

    complete script

--- auto-delpoy.sh
+++ auto-delpoy.sh
@@ -4,9 +4,14 @@ pid=$(ps aux | grep main.py | head -n 1 | cut -f7 -d" ")
 
 echo "got PID: $pid"
 
+echo "started killing bot: $pid"
 kill $pid
 echo "killed proccess: $pid"
 
-echo "booting Bot
-"
+echo "starting git pull"
+git pull
+echo "git pull done"
+
+echo "booting Bot"
+
 nohup python3 main.py

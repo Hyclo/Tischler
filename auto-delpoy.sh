1a9dbdc44b04f224954f3bba38d87a13a779ffe0
Miguel Aroldi <armi@aeis.ch>
2023-04-20 11:55:50 +0200

    lehrer fixed auto deploy

--- auto-delpoy.sh
+++ auto-delpoy.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# Check if process name argument was provided
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <process name>"
-    exit 1
-fi
+pid=ps aux | grep main.py | head -n 1 | cut -f7 -d" "
 
-# Find PID of the process
-pid=$(pgrep "$1")
-
-# Check if process exists
-if [ -z "$pid" ]; then
-    echo "Process not found: $1"
-    exit 1
-fi
-
-# Kill the process
-echo "Killing process $1 with PID $pid..."
-kill "$pid"
+echo "$pid"

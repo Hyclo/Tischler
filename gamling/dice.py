a6af1065f81295a3ebab7b509face8117b0787f9
Hyclo <miguel-aroldi@bluewin.ch>
2023-04-21 18:07:35 +0200

    fixed dice response

--- gamling/dice.py
+++ gamling/dice.py
@@ -21,6 +21,8 @@ async def dice(ctx, value):
             description="Just kidding " + ctx.author.mention + " we threw the same so lets keep what we bet!",
             color=discord.Colour.dark_grey()
         )
+        embed.add_field(name="Schreiners dice", value=str(schreiner_value))
+        embed.add_field(name="Your dice", value=str(user_value))
     elif user_value > schreiner_value:
         embed = discord.Embed(
             title="You won!",
@@ -28,6 +30,9 @@ async def dice(ctx, value):
             color=discord.Colour.green()
         )
         distributioner.add(ctx.author.id, "money", value)
+        embed.add_field(name="Schreiners dice", value=str(schreiner_value))
+        embed.add_field(name="Your dice", value=str(user_value))
+        embed.add_field(name="Your profit", value=str(value))
     else:
         embed = discord.Embed(
             title="402 Payment Required",
@@ -35,11 +40,10 @@ async def dice(ctx, value):
             color=discord.Colour.dark_red()
         )
         await distributioner.subtract(ctx.author.id, "money", value)
+        embed.add_field(name="Schreiners dice", value=str(schreiner_value))
+        embed.add_field(name="Your dice", value=str(user_value))
+        embed.add_field(name="Your loss", value=str(value))
         
-    
-    
-    embed.add_field(name="Schreiners dice", value=str(schreiner_value))
-    embed.add_field(name="Your dice", value=str(user_value))
     
     await ctx.respond(embed=embed)
     

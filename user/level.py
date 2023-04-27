9fa0d93d3ba4d234a9364478b4e76d6a82a36c8b
Hyclo <miguel-aroldi@bluewin.ch>
2023-04-27 19:43:49 +0200

    fixed get context parameters

--- user/level.py
+++ user/level.py
@@ -45,5 +45,5 @@ async def level(author_id, message, bot):
             
             distributioner.add(author_id, "level", 1)
             
-            ctx = await bot.get_application_context(interaction=message.interaction, cls=discord.ApplicationContext)
+            ctx = await discord.Bot.get_application_context(self=bot, interaction=message.interaction, cls=discord.ApplicationContext)
             ctx.respond("You have reached a new Level", ephemeral=True)

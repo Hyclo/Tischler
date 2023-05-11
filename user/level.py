601d70aab1e08a913b26dfa38af83ce99d39424a
Miguel Aroldi <armi@aeis.ch>
2023-05-11 15:33:09 +0200

    added rank command

--- user/level.py
+++ user/level.py
@@ -91,5 +91,5 @@ async def rank(ctx, member, bot):
     
     embed.add_field(name="Level", value=str(level), inline=True)
     embed.add_field(name="Experience", value=str(exp), inline=True)
-    embed.add_field(name="Progress to next level", value="[" + chr(0x00002588) * piece + " " * rest + "] " + str(percent_to_next_lvl) + "%", inline=False)
+    embed.add_field(name="Progress to next level", value="[" + chr(0x00002588) * int(piece) + " " * int(rest) + "] " + str(percent_to_next_lvl) + "%", inline=False)
             

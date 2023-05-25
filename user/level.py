eb6215875044ec5e8cb520c39ccc4b19db28126b
Miguel Aroldi <armi@aeis.ch>
2023-05-25 12:37:43 +0200

    edited text of edited response

--- user/level.py
+++ user/level.py
@@ -63,7 +63,7 @@ async def level(author_id, message, bot):
 
 async def rank(ctx, member, bot):
 
-    message = await ctx.respond(ctx.author.mention + "your level is being calculated...")
+    message = await ctx.respond(ctx.author.mention + " your level is being calculated...")
     
     ctx_author = ctx.author
 
@@ -90,4 +90,4 @@ async def rank(ctx, member, bot):
     
     file = discord.File("././output.png")
     
-    await message.edit_original_response(content="Complete", file=file)
+    await message.edit_original_response(content= ctx.author.mention + " calculation complete!", file=file)

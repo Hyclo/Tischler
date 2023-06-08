7a329b1337eb034fc1b2af8eddad3864e1e32722
Miguel Aroldi <armi@aeis.ch>
2023-06-08 14:55:24 +0200

    changes

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def on_message(message):
 async def backup():
     subprocess.call(['bash', './backup/backup.sh'])
 
-@tasks.loop(hseconds=1.0)
+@tasks.loop(seconds=1.0)
 async def stocks():
     await stocks_job(bot)
 

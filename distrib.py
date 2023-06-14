e463923f00c10e0836600da86d31ec36ce99fff3
Miguel Aroldi <armi@aeis.ch>
2023-06-14 09:35:50 +0200

    added new distrib file that has all the power to distribute values

---
+++ distrib.py
@@ -0,0 +1,68 @@
+'''
+def update(user_id, key, value):
+
+def add(user_id, key, value):
+
+def get_user(user_id):
+
+def get(user_id, key):
+
+async def subtract(user_id, key, value):
+
+async def get_multiple(key, reversed):
+'''
+
+def get_user(user_id):
+    return
+
+def get_all_users():
+    return
+
+def add_user(user_id):
+    return
+
+def delete_user(user_id):
+    return
+
+def get_stock(stock_name):
+    return
+
+def get_all_stocks():
+    return
+
+def add_stock(stock_name, stock_value):
+    return
+
+def add(user_id, key, value):
+    return
+
+def subtract(user_id, key, value):
+    return
+
+def update(user_id, key, value):
+    return
+
+def get_request(requestee_id, requested_id):
+    return
+
+def delete_request(requestee_id, requested_id):
+    return
+
+def add_request(requestee_id, requested_id):
+    return
+
+def get_buyer(buyer_id, stock_name):
+    return
+
+def delete_buyer(buyer_id, stock_name):
+    return
+
+def add_buyer(buyer_id, stock_name, stock_amount):
+    return
+
+def add_buyer_stock(buyer_id, stock_name):
+    return
+
+def subtract_buyer_stock(buyer_id, stock_name):
+    return
+

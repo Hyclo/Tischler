705df440674693a5c33c37b6f6887401139f701e
AresAndStuff <ares.dovalo@gmail.com>
2023-06-14 15:48:08 +0200

    migrated

--- work/work.py
+++ work/work.py
@@ -1,5 +1,5 @@
 import discord
-import distributioner
+import distrib
 import datetime
 import math
 import pentester
@@ -17,15 +17,15 @@ def get_difference_in_hours(date_1):
     
     start = datetime.datetime.strptime(date_1, date_format_str)
     end =   datetime.datetime.strptime(now, date_format_str)
-    # Get interval between two timstamps as timedelta object
+    # Get interval between two timestamps as timedelta object
     diff = end - start
-    # Get interval between two timstamps in hours
+    # Get interval between two timestamps in hours
     diff_in_hours = diff.total_seconds() / 3600
     
     return diff_in_hours
 
 async def check_time(ctx):
-    if get_difference_in_hours(distributioner.get(ctx.author.id, "timestampwork")) < 24:
+    if get_difference_in_hours(distrib.get_user(ctx.author.id).timestampwork) < 24:
         embed = discord.Embed(
             title="406 Not Acceptable",
             description= "Just kidding " + ctx.author.mention + " but you have to take a 24 hour break",
@@ -44,7 +44,7 @@ async def start_working(ctx):
     if await check_time(ctx) == False:
         return
     
-    if distributioner.get(ctx.author.id, "working") == "True":
+    if distrib.get_user(ctx.author.id).working == "True":
         embed = discord.Embed(
             title="406 Not Acceptable",
             description= "Just kidding " + ctx.author.mention + " but u are already working!",
@@ -57,8 +57,8 @@ async def start_working(ctx):
         
         now = datetime.datetime.strftime(now, "%Y-%m-%d %H:%M")
         
-        distributioner.update(ctx.author.id, "working", str(True))
-        distributioner.update(ctx.author.id, "timestampwork", now)
+        distrib.update(ctx.author.id, "working", str(True))
+        distrib.update(ctx.author.id, "timestampwork", now)
         
         embed = discord.Embed(
             title="Tischler",
@@ -73,7 +73,7 @@ async def end_working(ctx):
     if await pentester.check_user(ctx, ctx.author) == False:
         return
     
-    if distributioner.get(ctx.author.id, "working") == "False":
+    if distrib.get_user(ctx.author.id).working == "False":
         embed = discord.Embed(
             title="406 Not Acceptable",
             description= "Just kidding " + ctx.author.mention + " but u have to work first to get paid!",
@@ -81,9 +81,9 @@ async def end_working(ctx):
         )
         await ctx.respond(embed=embed)
     else:
-        worked_hours = round_up(get_difference_in_hours(distributioner.get(ctx.author.id, "timestampwork")), 2)
+        worked_hours = round_up(get_difference_in_hours(distrib.get_user(ctx.author.id).timestampwork), 2)
 
-        money = worked_hours * 10 * ((distributioner.get(ctx.author.id, "level") + 1) / 10)
+        money = worked_hours * 10 * ((distrib.get_user(ctx.author.id).level + 1) / 10)
         
         money = round_up(money, 0)
         
@@ -98,11 +98,11 @@ async def end_working(ctx):
         
         now = datetime.datetime.strftime(now, "%Y-%m-%d %H:%M")
         
-        distributioner.update(ctx.author.id, "timestampwork", now)
+        distrib.update(ctx.author.id, "timestampwork", now)
         
-        distributioner.update(ctx.author.id, "working", str(False))
+        distrib.update(ctx.author.id, "working", str(False))
         
-        distributioner.add(ctx.author.id, "money", money)
+        distrib.add_up_value(ctx.author.id, "money", money)
 
         embed.add_field(name="Hours worked", value=str(worked_hours) + "h", inline=True)
         
